899f53f846a621fb25a5e1afba0fbf8a64d6c141
Soumya0587 <ghoshalsoumalya96@gmail.com>
2022-12-17 13:50:13 +0530

    update

--- friday.com/src/AuthContext/Context.jsx
+++ friday.com/src/AuthContext/Context.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { createContext,useState } from "react";
 
-const Context = () => {
-  return (
-    <div>Context</div>
-  )
+export const AuthContext=createContext()
+
+
+
+function AuthContextProvider({children}) {
+    const obj={token:null,isAuth:false}
+    const [authState,setauth]=useState(obj)
+
+    const loginUser=(token)=>{
+        setauth({token:token,isAuth:true})
+    }
+    const logoutUser=()=>{
+        setauth(obj)
+    }
+    return <AuthContext.Provider value={{authState,loginUser,logoutUser}}>{children}</AuthContext.Provider>
+
 }
 
-export default Context
+export default AuthContextProvider;
